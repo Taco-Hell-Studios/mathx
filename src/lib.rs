30bfe7ce5db632f3e6e98192df7488e9120959cd
fulagann <fulagann@gmail.com>
2023-04-20 01:36:52 -0700

    Feat: Add feature to exclude parts of the library from being compiled
    This is to give the user an option to optimize for size when its for things they might not be using

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,17 @@
 mod math;
 pub use math::Math;
 
+#[cfg(not(feature = "no_vectors"))]
 mod arithmetic;
+#[cfg(not(feature = "no_vectors"))]
 pub(crate) use arithmetic::*;
 
+#[cfg(not(feature = "no_vectors"))]
 mod vectors;
+#[cfg(not(feature = "no_vectors"))]
 pub use vectors::{Vector3, Vector2};
 
+#[cfg(not(feature = "no_colors"))]
 mod colors;
+#[cfg(not(feature = "no_colors"))]
 pub use colors::Color;
