4a03bfb7eafbd318398c44ffda4c383740e1810f
Pablo GB <paul.gonzalez@bencinstudios.com>
2023-08-20 01:40:28 -0700

    Fix: Remove test configuration for CI

--- src/math.rs
+++ src/math.rs
@@ -1714,7 +1714,6 @@ impl Math {
 
 #[doc(hidden)]
 #[macro_export]
-#[cfg(test)]
 macro_rules! assert_range {
 	($expected:expr, $value:expr) => {
 		assert_range!($expected, $value, 0.0001);
@@ -1726,7 +1725,6 @@ macro_rules! assert_range {
 
 #[doc(hidden)]
 #[macro_export]
-#[cfg(test)]
 macro_rules! assert_range_tuple2 {
 	($expected:expr, $value:expr, $epsilon:expr) => {
 		if !Math::approx_epsilon($expected.0, $value.0, $epsilon) || !Math::approx_epsilon($expected.1, $value.1,  $epsilon) { panic!("\n\nleft: {:?}\nright: {:?}\n\n", $expected, $value); }
